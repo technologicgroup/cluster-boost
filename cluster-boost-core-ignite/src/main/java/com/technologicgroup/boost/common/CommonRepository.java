1f7361493a7411f030adb44c0a2fc3f81fd3a54e
Leonid Baranov <leonid@technologicgroup.com>
2020-05-31 21:06:27 +0200

    Code refactor

--- cluster-boost-core-ignite/src/main/java/com/technologicgroup/boost/common/CommonRepository.java
+++ cluster-boost-core-ignite/src/main/java/com/technologicgroup/boost/common/CommonRepository.java
@@ -8,26 +8,91 @@ import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.cache.affinity.Affinity;
+import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.ParameterizedType;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import javax.cache.Cache;
 
-@Repository
 public abstract class CommonRepository<K, V> implements ClusterRepository<K, V> {
 
   private CacheConfiguration<K, V> configuration;
 
   @Autowired
   private Ignite ignite;
+
+  @Override
+  public String getName() {
+    return getKeyClass().getSimpleName() + getValueClass().getSimpleName() + "Cache";
+  }
+
+  @Override
+  public ClusterGroup getClusterGroup(ClusterRepository<K, V> repository) {
+    CommonRepository<K, V> commonRepository = (CommonRepository<K, V>)repository;
+
+    org.apache.ignite.cluster.ClusterGroup
+        clusterGroup = ignite.cluster().forCacheNodes(commonRepository.cache().getName());
+
+    List<String> idList = clusterGroup.nodes()
+            .stream()
+            .map(ClusterNode::id)
+            .map(UUID::toString)
+            .collect(Collectors.toList());
+
+    return new ClusterGroup(idList);
+  }
+
+  @Override
+  public ClusterGroup getClusterGroup(ClusterRepository<K, V> repository, K key) {
+    Affinity<K> affinity = ignite.affinity(cache().getName());
+    return new ClusterGroup(Collections.singletonList(affinity.mapKeyToNode(key).id().toString()));
+  }
+
+  @Override
+  public V getLocal(K key) {
+    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
+      if (entry.getKey().equals(key)) {
+        return entry.getValue();
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public Map<K, V> getLocal(Set<K> keys) {
+    Map<K, V> result = new HashMap<>();
+
+    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
+      if (keys.contains(entry.getKey())) {
+        result.put(entry.getKey(), entry.getValue());
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public Map<K, V> getAllLocal() {
+    Map<K, V> result = new HashMap<>();
+    
+    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+    return result;
+  }
+
+  @Override
+  public void put(K key, V value) {
+    cache().put(key, value);
+  }
+
+  @Override
+  public void putAll(Map<K, V> map) {
+    cache().putAll(map);
+  }
 
   synchronized CacheConfiguration<K, V> getConfiguration() {
     if (configuration == null) {
@@ -37,16 +102,12 @@ public abstract class CommonRepository<K, V> implements ClusterRepository<K, V> 
   }
 
   protected CacheConfiguration<K, V> createCacheConfiguration() {
-      CacheConfiguration<K, V> result = new CacheConfiguration<>();
-      result.setCacheMode(CacheMode.PARTITIONED);
-      result.setName(getName());
-      result.setIndexedTypes(getKeyClass(), getValueClass());
+    CacheConfiguration<K, V> result = new CacheConfiguration<>();
+    result.setCacheMode(CacheMode.PARTITIONED);
+    result.setName(getName());
+    result.setIndexedTypes(getKeyClass(), getValueClass());
 
-      return result;
-  }
-
-  public String getName() {
-    return getKeyClass().getSimpleName() + getValueClass().getSimpleName() + "Cache";
+    return result;
   }
 
   @SuppressWarnings("unchecked")
@@ -65,61 +126,6 @@ public abstract class CommonRepository<K, V> implements ClusterRepository<K, V> 
 
   private Iterable<Cache.Entry<K, V>> getAllLocalIterable() {
     return cache().localEntries(CachePeekMode.ALL);
-  }
-
-  public ClusterGroup getClusterGroup(ClusterRepository<K, V> repository) {
-    CommonRepository<K, V> commonRepository = (CommonRepository<K, V>)repository;
-
-    org.apache.ignite.cluster.ClusterGroup
-        clusterGroup = ignite.cluster().forCacheNodes(commonRepository.cache().getName());
-
-    return new ClusterGroup(clusterGroup.nodes().stream().map(n -> n.id().toString()).collect(Collectors
-                                                                                                  .toList()));
-  }
-
-  @Override
-  public ClusterGroup getClusterGroup(ClusterRepository<K, V> repository, K key) {
-    Affinity<K> affinity = ignite.affinity(cache().getName());
-    return new ClusterGroup(Collections.singletonList(affinity.mapKeyToNode(key).id().toString()));
-  }
-
-  public V getLocal(K key) {
-    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
-      if (entry.getKey().equals(key)) {
-        return entry.getValue();
-      }
-    }
-
-    return null;
-  }
-
-  public Map<K, V> getLocal(Set<K> keys) {
-    Map<K, V> result = new HashMap<>();
-
-    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
-      if (keys.contains(entry.getKey())) {
-        result.put(entry.getKey(), entry.getValue());
-      }
-    }
-
-    return result;
-  }
-
-  public Map<K, V> getAllLocal() {
-    Map<K, V> result = new HashMap<>();
-    for (Cache.Entry<K, V> entry : getAllLocalIterable()) {
-      result.put(entry.getKey(), entry.getValue());
-    }
-
-    return result;
-  }
-
-  public void put(K key, V value) {
-    cache().put(key, value);
-  }
-
-  public void putAll(Map<K, V> map) {
-    cache().putAll(map);
   }
 
 }
