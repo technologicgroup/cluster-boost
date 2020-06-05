523c089eda764015b8e8265b2aa05ed1151de511
Leonid Baranov <leonid@technologicgroup.com>
2020-06-05 16:23:16 +0200

    Introduce new audit module

---
+++ cluster-boost-ignite-audit/src/main/java/com/technologicgroup/boost/audit/ClusterAuditConfig.java
@@ -0,0 +1,9 @@
+package com.technologicgroup.boost.audit;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClusterAuditConfig {
+
+}
